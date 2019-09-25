e1ed8157c9d9c7f5c69b3b45be957e4df0bf2b5b
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-09-25 20:16:46 +0000

    [feature] created char *_strdup(char *str) - closes #10

---
+++ _strdup.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * _strdup - Function that returns a pointer to a newly allocated memory
+ * @str: String to be allocated
+ * Return: Return a string allocated or NULL if it fails
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	char *new;
+
+	if (str == NULL)
+		return (NULL);
+
+	i = _strlen(str);
+
+	new = calloc(i + 1, sizeof(char));
+	if (new == NULL)
+	{
+		print_error(1);
+		exit(98);
+	}
+
+	for (i = 0; i < str[i]; i++)
+		new[i] = str[i];
+
+	new[i] = '\0';
+
+	return (new);
+}
