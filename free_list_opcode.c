2d39f484fe1018dc88e17ebf488993595e05a0b7
Hugo Vargas <vargas88hugo@gmail.com>
2019-09-25 13:54:08 -0500

    [feature] created free_list_opcode.c - closes #9

---
+++ free_list_opcode.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * free_list_opcode - Function that frees the opcode linked list
+ * @head: Head of the linked list
+ */
+void free_list_opcode(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->inst->opcode);
+		free(temp->inst);
+		free(temp);
+	}
+}
