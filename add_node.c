944945a3024feffffd0fc314bdba80d9a4c8c013
Hugo Vargas <vargas88hugo@gmail.com>
2019-09-26 18:17:06 -0500

    [fix] missing error message

--- add_node.c
+++ add_node.c
@@ -12,7 +12,13 @@ stack_t *add_nodeint(stack_t **head, const int n)
 	stack_t *new;
 
 	if (!head)
-		return (NULL);
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free_list_opcode(list_opcode);
+		if (head != NULL)
+			free_list_stack(*head);
+		exit(EXIT_FAILURE);
+	}
 
 	new = malloc(sizeof(stack_t));
 	if (new == NULL)
